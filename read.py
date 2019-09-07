768e834f6b3130808e59fe40d25a1aa480dbab7e
Jason Hung <brook110413@gmail.com>
2019-09-07 17:27:28 +0800

    filter good

--- read.py
+++ read.py
@@ -12,3 +12,17 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new), '筆資料小於100個字')
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有',len(good), '筆資料提到good')
+print(good[0])
